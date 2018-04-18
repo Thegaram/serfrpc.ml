2dcf502c2fddc1b040ba04f316f4c1d4170c91e0
Peter Garamvolgyi <peter.garamvolgyi@hotmail.com>
2018-04-18 16:15:09 +0200

    Use lwt

--- test/test.ml
+++ test/test.ml
@@ -1,44 +1,53 @@
-let send_request ?body (ic, oc) header =
-  let buffer_size = 4096 in
-  let obuffer = Buffer.create buffer_size in
-  let ibuffer = Bytes.create buffer_size in
+open Lwt.Infix
 
-  ignore (Msgpck.BytesBuf.write obuffer header);
-  (match body with
-    | None -> ()
-    | Some b -> ignore (Msgpck.BytesBuf.write obuffer b));
+let buffer_size = 4096
+let buffer = Bytes.create buffer_size
 
-  Buffer.output_buffer oc obuffer;
-  flush oc;
+let send_request ?body oc command seq =
+  let header = Request.Header.to_msgpack { command; seq } in
 
-  ignore (input ic ibuffer 0 buffer_size);
-  let (pos, header) = Msgpck.Bytes.read ibuffer in
-  let (_, body) = Msgpck.Bytes.read ~pos ibuffer in
+  let header_length = Msgpck.Bytes.write buffer header in
+  let body_length = match body with
+    | None -> 0
+    | Some b -> Msgpck.Bytes.write ~pos:header_length buffer b
+  in
+  let request_length = header_length + body_length in
 
-  (header, body)
+  let%lwt _ = Lwt_io.write_from oc buffer 0 request_length in
+  Lwt_io.flush oc
 
-let _ =
-  let sockaddr = Unix.ADDR_INET (Unix.inet_addr_of_string "127.0.0.1", 8082) in
-  let (ic, oc) = Unix.open_connection sockaddr in
+let read_response ic =
+  let%lwt _ = Lwt_io.read_into ic buffer 0 buffer_size in
 
-  let header = Request.Header.to_msgpack { command = Request.Command.Handshake; seq = 1 } in
+  let (pos, header) = Msgpck.Bytes.read buffer in
+  let (_, body) = Msgpck.Bytes.read ~pos buffer in
+
+  Lwt.return (header, body)
+
+let perform_request ?body ?callback (ic, oc) command seq =
+  send_request ?body oc command seq >>= fun () ->
+  read_response ic >|= fun (header, body) ->
+
+  print_endline (Msgpck.show header);
+
+  (match callback with
+    | Some callback -> callback body
+    | None -> ())
+
+let test =
+
+  let address = Unix.ADDR_INET (Unix.inet_addr_of_string "127.0.0.1", 8082) in
+  let%lwt (ic, oc) = Lwt_io.open_connection address in
+
   let body = Request.Handshake.to_msgpack { version = 1 } in
-  let (h, b) = send_request (ic, oc) header ?body:(Some body) in
-  print_endline (Msgpck.show h);
+  let%lwt _ = perform_request (ic, oc) Request.Command.Handshake 1 ~body in
 
-  let header = Request.Header.to_msgpack { command = Request.Command.Event; seq = 2 } in
   let body = Request.Event.to_msgpack { name = "hey!"; payload = ""; coalesce = false } in
-  let (h, b) = send_request (ic, oc) header ?body:(Some body) in
-  print_endline (Msgpck.show h);
+  let%lwt _ = perform_request (ic, oc) Request.Command.Event 2 ~body in
 
-  let header = Request.Header.to_msgpack { command = Request.Command.Members; seq = 3 } in
-  let (h, b) = send_request (ic, oc) header ?body:None in
-  print_endline (Msgpck.show h);
-  print_endline (Msgpck.show b);
+  let callback = fun b -> print_endline (Msgpck.show b) in
+  let%lwt _ = perform_request (ic, oc) Request.Command.Members 3 ~callback in
 
-  let header = Request.Header.to_msgpack { command = Request.Command.Stats; seq = 4 } in
-  let (h, b) = send_request (ic, oc) header ?body:None in
-  print_endline (Msgpck.show h);
-  print_endline (Msgpck.show b);
+  Lwt_io.close ic
 
-  Unix.shutdown_connection ic;
+let _ = Lwt_main.run test
