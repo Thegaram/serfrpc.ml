bd18fe257ea57245bb0083d0829b664f295c58bb
Peter Garamvolgyi <peter.garamvolgyi@hotmail.com>
2018-04-18 16:38:51 +0200

    Read responses in separate thread

--- test/test.ml
+++ test/test.ml
@@ -1,5 +1,7 @@
 open Lwt.Infix
 
+let my_hash : (int, Msgpck.t -> unit) Hashtbl.t = (Hashtbl.create 1000)
+
 let buffer_size = 4096
 let buffer = Bytes.create buffer_size
 
@@ -16,28 +18,30 @@ let send_request ?body oc command seq =
   let%lwt _ = Lwt_io.write_from oc buffer 0 request_length in
   Lwt_io.flush oc
 
-let read_response ic =
-  let%lwt _ = Lwt_io.read_into ic buffer 0 buffer_size in
-
-  let (pos, header) = Msgpck.Bytes.read buffer in
-  let (_, body) = Msgpck.Bytes.read ~pos buffer in
-
-  Lwt.return (header, body)
+let rec read_loop ic =
+  let read_response ic =
+    let%lwt _ = Lwt_io.read_into ic buffer 0 buffer_size in
+    let (pos, header) = Msgpck.Bytes.read buffer in
+    let (_, body) = Msgpck.Bytes.read ~pos buffer in
+    Lwt.return (header, body)
+  in
+  let%lwt (h, b) = read_response ic in
+  let header = Response.Header.from_msgpack h in
+  let seq = header.seq in
+  let callback = Hashtbl.find my_hash seq in
+  callback b;
+  read_loop ic
 
 let perform_request ?body ?callback (ic, oc) command seq =
-  send_request ?body oc command seq >>= fun () ->
-  read_response ic >|= fun (header, body) ->
-
-  print_endline (Msgpck.show header);
-
-  (match callback with
-    | Some callback -> callback body
-    | None -> ())
+  let callback = match callback with | Some c -> c | None -> (fun _ -> ()) in
+  Hashtbl.add my_hash seq callback;
+  send_request ?body oc command seq
 
 let test =
-
   let address = Unix.ADDR_INET (Unix.inet_addr_of_string "127.0.0.1", 8082) in
   let%lwt (ic, oc) = Lwt_io.open_connection address in
+
+  ignore (Lwt_preemptive.detach read_loop ic);
 
   let body = Request.Handshake.to_msgpack { version = 1 } in
   let%lwt _ = perform_request (ic, oc) Request.Command.Handshake 1 ~body in
@@ -47,6 +51,10 @@ let test =
 
   let callback = fun b -> print_endline (Msgpck.show b) in
   let%lwt _ = perform_request (ic, oc) Request.Command.Members 3 ~callback in
+
+  let%lwt _ = perform_request (ic, oc) Request.Command.Members 4 ~callback in
+
+  let%lwt () = Lwt_unix.sleep 1.0 in
 
   Lwt_io.close ic
 
